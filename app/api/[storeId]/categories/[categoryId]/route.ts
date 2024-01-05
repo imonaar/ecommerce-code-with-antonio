4a49d59d5998bf039422a1765039a4e361be9802
imonaar <kevinmonari22@gmail.com>
2024-01-05 10:42:57 +0300

    stripe complete & working

--- app/api/[storeId]/categories/[categoryId]/route.ts
+++ app/api/[storeId]/categories/[categoryId]/route.ts
@@ -128,6 +128,9 @@ export async function GET(
             where: {
                 id: params.categoryId
             },
+            include: {
+                billboard: true
+            }
         });
 
         return NextResponse.json(category)
